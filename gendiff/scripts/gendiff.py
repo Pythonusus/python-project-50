68cf1868b374645fbbfb849d448631f0dc882592
Petr Malafeev <StandBy545@yandex.ru>
2024-03-10 14:24:03 +0300

    Move argparse logic from gendiff script to cli

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 """
 """
-import argparse
+from gendiff import cli
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description=('Compares two configuration files '
-                     'and shows a difference.')
-    )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    args = parser.parse_args()
+    cli.args
 
 
 if __name__ == '__main__':
