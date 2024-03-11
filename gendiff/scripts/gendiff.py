e0a621e3272256343dc9132ab33bdab978b582a9
Petr Malafeev <StandBy545@yandex.ru>
2024-03-11 07:32:01 +0300

    Upd gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
 """
-from gendiff import cli
+from gendiff.cli import args
+from gendiff.gendiff import generate_diff
 
 
 def main():
-    cli.args
+    file1_path, file2_path = args.first_file, args.second_file
+    print(generate_diff(file1_path, file2_path))
 
 
 if __name__ == '__main__':
