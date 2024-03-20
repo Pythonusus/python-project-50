9df0eb84a2be73184a864b04d22941e4c9ba1f64
Petr Malafeev <StandBy545@yandex.ru>
2024-03-20 18:29:34 +0300

    Upd test_gendiff.py
    Add yaml testing function.

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -1,10 +1,26 @@
 from gendiff.gendiff import generate_diff
 
 
-def test_gendiff():
+def test_gendiff_json():
     assert generate_diff(
         'gendiff/tests/fixtures/test_file1.json',
         'gendiff/tests/fixtures/test_file2.json'
+    ) == (
+        '{\n'
+        ' - follow: false\n'
+        ' host: hexlet.io\n'
+        ' - proxy: 123.234.53.22\n'
+        ' - timeout: 50\n'
+        ' + timeout: 20\n'
+        ' + verbose: true\n'
+        '}'
+    )
+
+
+def test_gendiff_yaml():
+    assert generate_diff(
+        'gendiff/tests/fixtures/test_file1.yaml',
+        'gendiff/tests/fixtures/test_file2.yml'
     ) == (
         '{\n'
         ' - follow: false\n'
