671b72df462e66788c61d7cd0b692f5c9712d2b9
Petr Malafeev <StandBy545@yandex.ru>
2024-03-20 11:33:56 +0300

    Fix gendiff module
    Add sorting to keys.
    Add edit_result_string function to edit generate_diff result string
    to proper json string.

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,12 +1,19 @@
 import json
 
 
+def edit_result_string(string):
+    edited_string = string.replace('True', 'true')
+    edited_string = edited_string.replace('False', 'false')
+    edited_string = edited_string.replace('None', 'null')
+    return edited_string
+
+
 def generate_diff(file_path1, file_path2):
     data1 = json.load(open(file_path1))  # dict
     data2 = json.load(open(file_path2))  # dict
     diffs = ['{']
 
-    keys = set().union(data1.keys(), data2.keys())
+    keys = sorted(set().union(data1.keys(), data2.keys()))
     for key in keys:
         value1 = data1.get(key)
         value2 = data2.get(key)
@@ -20,4 +27,5 @@ def generate_diff(file_path1, file_path2):
             diffs.append(f' - {key}: {data1[key]}')
             diffs.append(f' + {key}: {data2[key]}')
     diffs.append('}')
-    return '\n'.join(diffs)
+    result = '\n'.join(diffs)
+    return edit_result_string(result)
