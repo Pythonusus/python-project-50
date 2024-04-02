17498926622e2d8e18b566270bb2226d8324f7fa
Petr Malafeev <StandBy545@yandex.ru>
2024-04-02 16:36:38 +0300

    Upd gendiff module
    compare_data function now only generates difference
    using python structures.
    Formatters are used to form an output from difference.

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,24 +3,37 @@ from gendiff.loaders import select_loader
 
 
 def compare_data(data1, data2):
-    diffs = ['{']
+    diff = dict()
 
     keys = sorted(set().union(data1.keys(), data2.keys()))
     for key in keys:
         value1 = data1.get(key)
         value2 = data2.get(key)
-        if value1 is None:  # d.get(keys) returns None if key not in d
-            diffs.append(f'  + {key}: {data2[key]}')
-        elif value2 is None:
-            diffs.append(f'  - {key}: {data1[key]}')
+        if isinstance(value1, dict) and isinstance(value2, dict):
+            nested_diff = compare_data(value1, value2)
+            diff[key] = nested_diff
+        elif value1 is None:  # d.get(keys) returns None if key not in d
+            diff[key] = {
+                'value': value2,
+                'status': 'added'
+            }
+        elif value2 is None:  # d.get(keys) returns None if key not in d
+            diff[key] = {
+                'value': value1,
+                'status': 'deleted'
+            }
         elif value1 == value2:
-            diffs.append(f'    {key}: {data1[key]}')
+            diff[key] = {
+                'value': value1,
+                'status': 'unchanged'
+            }
         else:
-            diffs.append(f'  - {key}: {data1[key]}')
-            diffs.append(f'  + {key}: {data2[key]}')
-    diffs.append('}')
-    result = '\n'.join(diffs)
-    return edit_result_string(result)
+            diff[key] = {
+                'value1': value1,
+                'value2': value2,
+                'status': 'modified'
+            }
+    return diff
 
 
 def generate_diff(file_path1, file_path2, formatter='stylish'):
@@ -31,3 +44,30 @@ def generate_diff(file_path1, file_path2, formatter='stylish'):
     diff = compare_data(data1, data2)
     format_data = select_formatter(formatter)
     return format_data(diff)
+
+# def edit_result_string(string):
+#     edited_string = string.replace('True', 'true')
+#     edited_string = edited_string.replace('False', 'false')
+#     edited_string = edited_string.replace('None', 'null')
+#     return edited_string
+
+
+# def compare_data(data1, data2):
+#     diffs = ['{']
+
+#     keys = sorted(set().union(data1.keys(), data2.keys()))
+#     for key in keys:
+#         value1 = data1.get(key)
+#         value2 = data2.get(key)
+#         if value1 is None:  # d.get(keys) returns None if key not in d
+#             diffs.append(f'  + {key}: {data2[key]}')
+#         elif value2 is None:
+#             diffs.append(f'  - {key}: {data1[key]}')
+#         elif value1 == value2:
+#             diffs.append(f'    {key}: {data1[key]}')
+#         else:
+#             diffs.append(f'  - {key}: {data1[key]}')
+#             diffs.append(f'  + {key}: {data2[key]}')
+#     diffs.append('}')
+#     result = '\n'.join(diffs)
+#     return edit_result_string(result)
