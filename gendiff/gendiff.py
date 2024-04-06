95528d0a55d7d3b0e5e1e25f3cbc2193caf161ee
Petr Malafeev <StandBy545@yandex.ru>
2024-04-06 17:52:44 +0300

    Delete unnecessary comments

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -11,30 +11,3 @@ def generate_diff(file_path1, file_path2, formatter='stylish'):
     diff = compare_data(data1, data2)
     format_data = select_formatter(formatter)
     return format_data(diff)
-
-# def edit_result_string(string):
-#     edited_string = string.replace('True', 'true')
-#     edited_string = edited_string.replace('False', 'false')
-#     edited_string = edited_string.replace('None', 'null')
-#     return edited_string
-
-
-# def compare_data(data1, data2):
-#     diffs = ['{']
-
-#     keys = sorted(set().union(data1.keys(), data2.keys()))
-#     for key in keys:
-#         value1 = data1.get(key)
-#         value2 = data2.get(key)
-#         if value1 is None:  # d.get(keys) returns None if key not in d
-#             diffs.append(f'  + {key}: {data2[key]}')
-#         elif value2 is None:
-#             diffs.append(f'  - {key}: {data1[key]}')
-#         elif value1 == value2:
-#             diffs.append(f'    {key}: {data1[key]}')
-#         else:
-#             diffs.append(f'  - {key}: {data1[key]}')
-#             diffs.append(f'  + {key}: {data2[key]}')
-#     diffs.append('}')
-#     result = '\n'.join(diffs)
-#     return edit_result_string(result)
