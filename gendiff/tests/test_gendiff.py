e215530f9f6b7bf6521ccf47730b713b26d14dc4
Petr Malafeev <StandBy545@yandex.ru>
2024-04-11 22:45:26 +0300

    Upd tests

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -13,13 +13,16 @@ EXPECTED_FLAT_STYLISH = 'gendiff/tests/fixtures/expected_flat_stylish.txt'
 EXPECTED_NESTED_STYLISH = 'gendiff/tests/fixtures/expected_nested_stylish.txt'
 EXPECTED_FLAT_PLAIN = 'gendiff/tests/fixtures/expected_flat_plain.txt'
 EXPECTED_NESTED_PLAIN = 'gendiff/tests/fixtures/expected_nested_plain.txt'
+EXPECTED_FLAT_JSON = 'gendiff/tests/fixtures/expected_flat.json'
+EXPECTED_NESTED_JSON = 'gendiff/tests/fixtures/expected_nested.json'
 
 
 @pytest.mark.parametrize('file_path1, file_path2, expected_file_path', [
     (FLAT1_JSON, FLAT2_JSON, EXPECTED_FLAT_STYLISH),
     (FLAT1_YAML, FLAT2_YAML, EXPECTED_FLAT_STYLISH),
     (NESTED1_JSON, NESTED2_JSON, EXPECTED_NESTED_STYLISH),
-    (NESTED1_YAML, NESTED2_YAML, EXPECTED_NESTED_STYLISH)])
+    (NESTED1_YAML, NESTED2_YAML, EXPECTED_NESTED_STYLISH),
+])
 def test_gendiff_stylish(file_path1, file_path2, expected_file_path):
     with open(expected_file_path) as expected:
         assert generate_diff(file_path1, file_path2) == expected.read()
@@ -29,9 +32,23 @@ def test_gendiff_stylish(file_path1, file_path2, expected_file_path):
     (FLAT1_JSON, FLAT2_JSON, EXPECTED_FLAT_PLAIN),
     (FLAT1_YAML, FLAT2_YAML, EXPECTED_FLAT_PLAIN),
     (NESTED1_JSON, NESTED2_JSON, EXPECTED_NESTED_PLAIN),
-    (NESTED1_YAML, NESTED2_YAML, EXPECTED_NESTED_PLAIN)])
+    (NESTED1_YAML, NESTED2_YAML, EXPECTED_NESTED_PLAIN),
+])
 def test_gendiff_plain(file_path1, file_path2, expected_file_path):
     with open(expected_file_path) as expected:
         assert generate_diff(
             file_path1, file_path2, formatter='plain'
+        ) == expected.read()
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_file_path', [
+    (FLAT1_JSON, FLAT2_JSON, EXPECTED_FLAT_JSON),
+    (FLAT1_YAML, FLAT2_YAML, EXPECTED_FLAT_JSON),
+    (NESTED1_JSON, NESTED2_JSON, EXPECTED_NESTED_JSON),
+    (NESTED1_YAML, NESTED2_YAML, EXPECTED_NESTED_JSON),
+])
+def test_gendiff_json(file_path1, file_path2, expected_file_path):
+    with open(expected_file_path) as expected:
+        assert generate_diff(
+            file_path1, file_path2, formatter='json'
         ) == expected.read()
