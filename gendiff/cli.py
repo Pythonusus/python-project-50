07d11ae1987a97a9baa1601d01aba51298c86f21
Petr Malafeev <StandBy545@yandex.ru>
2024-04-15 22:35:10 +0300

    Refactor cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,14 +1,17 @@
 import argparse
 
-parser = argparse.ArgumentParser(
-    description=('Compares two configuration files '
-                 'and shows a difference.')
-)
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument(
-    '-f', '--format',
-    help='set format of output (default: stylish)',
-    default='stylish'
-)
-args = parser.parse_args()
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=('Compares two configuration files '
+                     'and shows a difference.')
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output (default: stylish)',
+        default='stylish'
+    )
+    args = parser.parse_args()
+    return args
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
 """
-from gendiff.cli import args
+from gendiff.cli import parse_args
 from gendiff.gendiff import generate_diff
 
 
 def main():
+    args = parse_args()
     file1_path, file2_path = args.first_file, args.second_file
     formatter = args.format
     print(generate_diff(file1_path, file2_path, formatter))
