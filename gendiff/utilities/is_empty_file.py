55787ff7d5fd05efd31fe537bc30d60a9344ef21
Petr Malafeev <StandBy545@yandex.ru>
2024-04-16 01:21:35 +0300

    Fix hexlet-check build failure issue

--- gendiff/utilities/is_empty_file.py
+++ gendiff/utilities/is_empty_file.py
@@ -1,5 +1,6 @@
-from pathlib import Path
+import os
 
 
 def is_empty_file(path_to_file):
-    return True if Path(path_to_file).stat().st_size == 0 else False
+    stats = os.stat(path_to_file)
+    return True if stats.st_size == 0 else False
