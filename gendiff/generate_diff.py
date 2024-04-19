eef5419ec9a901aec36b644bc0a1398b5752bb83
Petr Malafeev <StandBy545@yandex.ru>
2024-04-19 19:53:39 +0300

    Upd generate_diff.py module
    Now load_file_data func is used to parse data from files.

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,13 +1,11 @@
 from gendiff.formatters.formatter_router import select_formatter
 from gendiff.utilities.data_comparator import compare_data
-from gendiff.utilities.loaders import select_loader
+from gendiff.utilities.loaders import load_file_data
 
 
 def generate_diff(file_path1, file_path2, formatter='stylish'):
-    file1_loader = select_loader(file_path1)
-    file2_loader = select_loader(file_path2)
-    data1 = file1_loader(file_path1)
-    data2 = file2_loader(file_path2)
+    data1 = load_file_data(file_path1)
+    data2 = load_file_data(file_path2)
     diff = compare_data(data1, data2)
     format_data = select_formatter(formatter)
     return format_data(diff)
